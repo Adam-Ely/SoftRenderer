ab9e39594bd6b6e69aa0f75eeff04f417848a7c3
Adam-Ely <speedfreak31@hotmail.com>
2017-05-26 15:47:26 +0100

    Added version of drawPoint that takes ints

--- SoftRenderer/LineDrawing.h
+++ SoftRenderer/LineDrawing.h
@@ -2,10 +2,8 @@
 #include "Vec3.h"
 #include "DrawWrapper.h"
 
-//extern const int SCREEN_WIDTH;
-//extern const int SCREEN_HEIGHT;
-
-void draw2DPoint(Vec3 * point);
+void drawPoint(int x, int y);
+void drawPoint(Vec3 * point);
 void drawLine(int x1, int y1, int x2, int y2);
 void drawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
 void drawTriangle(Vec3 *first, Vec3 *second, Vec3 *third);
